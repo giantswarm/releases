f37635a84cb0dda3356b7a5232aeed1ba0f76564
Zach Stone <zach@giantswarm.io>
2020-07-24 11:36:23 +0200

    Add the key file...

---
+++ test_key.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	releaseFilename       = "release.yaml"
+	readmeFilename        = "README.md"
+	kustomizationFilename = "kustomization.yaml"
+	requestsFilename      = "requests.yaml"
+)
