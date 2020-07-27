194b8c708c8e2064b80d48449116276d5eb4377f
Zach Stone <zach@giantswarm.io>
2020-07-27 17:52:32 +0200

    Check that releases satisfy requested component versions (#373)
    * WIP
    
    * More WIP
    
    * Cleanup and remove requests for initial review
    
    * Add some punctuation
    
    * Move filenames to key.go
    
    * Add the key file...
    
    * Alphabetize
    
    * Check apps as well
    
    * Comment
    
    * Bump Masterminds/semver to v3
    
    * Rename key file

---
+++ key.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	kustomizationFilename = "kustomization.yaml"
+	readmeFilename        = "README.md"
+	releaseFilename       = "release.yaml"
+	requestsFilename      = "requests.yaml"
+)
